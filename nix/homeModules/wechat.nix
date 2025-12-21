3d004edb46b08860427c798b1cb64a71f8daaad3
tsx8 <tangsongxiaoba@163.com>
2025-12-21 12:25:49 +0800

    ci: Add wechat-stub to prevent checking it in Actions.

--- nix/homeModules/wechat.nix
+++ nix/homeModules/wechat.nix
@@ -1,16 +1,22 @@
 { pkgs, ... }:
 
 let
+  isCI = (builtins.getEnv "GITHUB_ACTIONS") == "true";
   wechatDesktopContent = builtins.readFile ../../configs/wechat/wechat.desktop;
+
+  wechatStub = pkgs.runCommand "wechat-stub" { } ''
+    mkdir -p $out/share/applications
+    echo '${wechatDesktopContent}' > $out/share/applications/wechat.desktop
+  '';
+
+  wechatReal = pkgs.wechat.overrideAttrs (oldAttrs: {
+    buildCommand = (oldAttrs.buildCommand or "") + ''
+      rm $out/share/applications/wechat.desktop
+      mkdir -p $out/share/applications
+      echo '${wechatDesktopContent}' > $out/share/applications/wechat.desktop
+    '';
+  });
 in
 {
-  home.packages = [
-    (pkgs.wechat.overrideAttrs (oldAttrs: {
-      buildCommand = (oldAttrs.buildCommand or "") + ''
-        rm $out/share/applications/wechat.desktop
-        mkdir -p $out/share/applications
-        echo '${wechatDesktopContent}' > $out/share/applications/wechat.desktop
-      '';
-    }))
-  ];
+  home.packages = [ (if isCI then wechatStub else wechatReal) ];
 }
